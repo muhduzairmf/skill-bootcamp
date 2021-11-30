ebfc9d8748ee7419d734bb8aeecaed307ded8ba1
muhduzairmf <uzair.developer@outlook.com>
2021-11-30 13:43:36 +0800

    first initialize

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+
+AOS.init();
+
+new Typewriter('#typewriter', {
+    strings: ['Information Technology', 'Religion Study', 'Mathematics', 'Literature & Languages', 'Philosophy', 'Engineering', 'Art & Design', 'History'],
+    autoStart: true,
+    loop: true,
+});
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('main', () => ({
+        
+    }));
+});
