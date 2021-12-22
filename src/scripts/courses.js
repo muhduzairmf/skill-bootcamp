38b9adedcc6508abbb6945d40c6f0772e00ce380
muhduzairmf <uzair.developer@outlook.com>
2021-12-22 10:38:14 +0800

    Make new changes

--- src/scripts/courses.js
+++ src/scripts/courses.js
@@ -23,5 +23,73 @@ document.addEventListener('alpine:init', () => {
         toggleSearchFilterModal() {
             document.querySelector('#searchFilterModal').classList.toggle('is-active');
         },
+        contentCard1: true,
+        arrowUp1: true,
+        arrowDown1: false,
+        contentCard2: false,
+        arrowUp2: false,
+        arrowDown2: true,
+        contentCard3: false,
+        arrowUp3: false,
+        arrowDown3: true,
+        toggleCardContent1() {
+            this.contentCard1 = ! this.contentCard1;
+            this.arrowUp1 = ! this.arrowUp1;
+            this.arrowDown1 = ! this.arrowDown1;
+        },
+        toggleCardContent2() {
+            this.contentCard2 = ! this.contentCard2;
+            this.arrowUp2 = ! this.arrowUp2;
+            this.arrowDown2 = ! this.arrowDown2;
+        },
+        toggleCardContent3() {
+            this.contentCard3 = ! this.contentCard3;
+            this.arrowUp3 = ! this.arrowUp3;
+            this.arrowDown3 = ! this.arrowDown3;
+        },
+        toggleCourseContent() {
+            document.getElementById('courseContent').classList.toggle('is-active');
+        },
+        comment1: false,
+        comment2: false,
+        toggleComment1() {
+            this.comment1 = ! this.comment1;
+        },
+        toggleComment2() {
+            this.comment2 = ! this.comment2;
+        },
+        userComment1: '',
+        clearComment1() {
+            this.userComment1 = '';
+        },
+        countComment1: 1,
+        doneComment1: '',
+        commenting1() {
+            this.countComment1++;
+            var datenow = dayjs().format('MMMM D[,] YYYY');
+            var timenow = dayjs().format('h[:]m A');
+            this.doneComment1 += `
+                <article class="media my-6">
+                    <figure class="media-left">
+                        <p class="image is-48x48">
+                        <img class="is-rounded" src="https://randomuser.me/api/portraits/men/40.jpg">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content poppins">
+                        <p>
+                            <strong class="is-size-5">Tim Carlton </strong>
+                            <br>
+                            <small class="subtitle is-7">${datenow} &nbsp;|&nbsp; ${timenow}</small>
+                            <br><br>
+                            ${DOMPurify.sanitize(this.userComment1)}
+                            <br>
+                        </p>
+                        </div>
+                    </div>
+                </article>
+            `;
+            this.userComment1 = '';
+        }
     }));
 });
