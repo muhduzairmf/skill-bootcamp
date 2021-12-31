4237533923be8fce2d2b76209e303833c778cdb2
muhduzairmf <uzair.developer@outlook.com>
2021-12-31 18:01:07 +0800

    Completing certain features

--- src/scripts/createCourse.js
+++ src/scripts/createCourse.js
@@ -1,3 +1,19 @@
+const fileInput = document.querySelector('#file-js input[type=file]');
+fileInput.onchange = () => {
+    if (fileInput.files.length > 0) {
+        const fileName = document.querySelector('#file-js .file-name');
+        fileName.textContent = fileInput.files[0].name;
+    }
+}
+
+const input_image = document.querySelector("#image_input");
+input_image.addEventListener("change", function() {
+    document.querySelector('#image_section').style.display = "block";
+    document.querySelector("#display_image").src = `https://images.pexels.com/photos/4300396/pexels-photo-4300396.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`;
+});
+
+var countControl = 1;
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('main', () => ({
         displayMenu() {
@@ -14,5 +30,208 @@ document.addEventListener('alpine:init', () => {
             window.location.href = '../../skill-bootcamp/index.html';
             //window.location.href = '../../index.html';
         },
+        overviewForm: '',
+        addOverview() {
+            this.overviewForm += `
+            <div class="field">
+                <div class="control">
+                    <input type="text" class="input poppins" maxlength="30" >
+                </div>
+            </div>
+            `;
+        },
+        chapter: '',
+        addChapter() {
+            countControl++;
+            this.chapter += `
+            <hr style="background-color: rgba(192, 192, 192, 0.37) !important;">
+            <p class="poppins has-text-centered"><strong>Chapter ${countControl}</strong></p>
+            <div class="columns is-vcentered mx-2 mt-1">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Chapter Title</p>
+                </div>
+                <div class="column is-9">
+                    <div class="field">
+                        <div class="control">
+                            <input type="text" class="input poppins" maxlength="30" placeholder="Enter a chapter title">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="chapter-divider poppins mb-5" style="margin-left: 100px; margin-right: 100px;">&nbsp; <small>Video</small> &nbsp;</div>
+            <div class="columns is-vcentered mx-2 mt-1">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Video Title</p>
+                </div>
+                <div class="column is-9">
+                    <div class="field">
+                        <div class="control">
+                            <input type="text" class="input poppins" maxlength="30" placeholder="Enter a course title">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="columns mx-2">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Description</p>
+                </div>
+                <div class="column is-9">
+                    <div class="field">
+                        <div class="control">
+                            <textarea class="textarea poppins" placeholder="Enter a description of the course"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="columns mx-2">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Video</p>
+                </div>
+                <div class="column is-9">
+                    <div class="file is-normal is-boxed has-name">
+                        <label class="file-label">
+                            <input class="file-input" type="file" name="thumbnail">
+                            <span class="file-cta">
+                            <span class="file-icon">
+                                <i class="bi bi-upload"></i>
+                            </span>
+                            <span class="file-label poppins">
+                                Choose a video
+                            </span>
+                            </span>
+                        </label>
+                    </div>
+                    <figure class="image mt-2 is-16by9" style="display: none;">
+                        <img src="" alt="">
+                    </figure>
+                </div>
+            </div>
+            <div class="chapter-divider poppins mb-5" style="margin-left: 100px; margin-right: 100px;">&nbsp; <small>Article</small> &nbsp;</div>
+            <div class="columns is-vcentered mx-2 mt-1">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Article Title</p>
+                </div>
+                <div class="column is-9">
+                    <div class="field">
+                        <div class="control">
+                            <input type="text" class="input poppins" maxlength="30" placeholder="Enter a article title">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="columns mx-2">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Description</p>
+                </div>
+                <div class="column is-9">
+                    <div class="field">
+                        <div class="control">
+                            <textarea class="textarea poppins" placeholder="Enter a description of the article"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="columns mx-2">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Article</p>
+                </div>
+                <div class="column is-9">
+                    <div class="file is-normal is-boxed has-name">
+                        <label class="file-label">
+                            <input class="file-input" type="file" name="thumbnail">
+                            <span class="file-cta">
+                            <span class="file-icon">
+                                <i class="bi bi-upload"></i>
+                            </span>
+                            <span class="file-label poppins">
+                                Choose a article in PDF
+                            </span>
+                            </span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="chapter-divider poppins mb-5" style="margin-left: 100px; margin-right: 100px;">&nbsp; <small>Quiz</small> &nbsp;</div>
+            <div class="columns is-vcentered mx-2 mt-1">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Quiz Title</p>
+                </div>
+                <div class="column is-9">
+                    <div class="field">
+                        <div class="control">
+                            <input type="text" class="input poppins" maxlength="30" placeholder="Enter a quiz title">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="columns mx-2">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Description</p>
+                </div>
+                <div class="column is-9">
+                    <div class="field">
+                        <div class="control">
+                            <textarea class="textarea poppins" placeholder="Enter a description of the quiz"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="columns is-vcentered mx-2">
+                <div class="column is-3">
+                    <p class="title is-size-6 poppins">Question 1</p>
+                </div>
+                <div class="column is-9">
+                    <input type="text" class="input poppins" placeholder="Enter a question">
+                </div>
+            </div>
+            <div class="columns is-vcentered mx-2">
+                <div class="column is-2"></div>
+                <div class="column is-1">
+                    <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                </div>
+                <div class="column is-9">
+                    <input type="text" class="input poppins">
+                </div>
+            </div>
+            <div class="columns is-vcentered mx-2">
+                <div class="column is-2"></div>
+                <div class="column is-1">
+                    <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                </div>
+                <div class="column is-9">
+                    <input type="text" class="input poppins">
+                </div>
+            </div>
+            <div class="columns is-vcentered mx-2">
+                <div class="column is-2"></div>
+                <div class="column is-1">
+                    <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                </div>
+                <div class="column is-9">
+                    <input type="text" class="input poppins">
+                </div>
+            </div>
+            <div class="columns is-vcentered mx-2">
+                <div class="column is-2"></div>
+                <div class="column is-1">
+                    <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                </div>
+                <div class="column is-9">
+                    <input type="text" class="input poppins">
+                </div>
+            </div>
+            <div class="level mt-4">
+                <div class="level-item">
+                    <button class="button is-small is-rounded has-background-grey-light has-text-black poppins">+ Add question</button>
+                </div>
+            </div>
+            `;
+        },
+        createCourse: true,
+        successCreate: false,
+        publishCourse() {
+            this.createCourse = ! this.createCourse;
+            this.successCreate = ! this.successCreate;
+        }
     }));
 });
--- src/scripts/createQuiz.js
+++ src/scripts/createQuiz.js
@@ -1,3 +1,19 @@
+var countControl = 1;
+
+const fileInput = document.querySelector('#file-js input[type=file]');
+fileInput.onchange = () => {
+    if (fileInput.files.length > 0) {
+        const fileName = document.querySelector('#file-js .file-name');
+        fileName.textContent = fileInput.files[0].name;
+    }
+}
+
+const image_input = document.querySelector("#image_input");
+image_input.addEventListener("change", function() {
+    document.querySelector('#image_section').style.display = "block";
+    document.querySelector("#display_image").src = `https://images.pexels.com/photos/4300396/pexels-photo-4300396.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`;
+});
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('main', () => ({
         displayMenu() {
@@ -14,5 +30,64 @@ document.addEventListener('alpine:init', () => {
             window.location.href = '../../skill-bootcamp/index.html';
             //window.location.href = '../../index.html';
         },
+        minutes: [0, 15, 30, 45],
+        hours: [0, 1, 2, 3],
+        question: '',
+        addQuestion() {
+            countControl++;
+            this.question += `
+                <br>
+                <div class="columns is-vcentered mx-2">
+                    <div class="column is-3">
+                        <p class="title is-size-6 poppins">Question <span>${countControl}</span></p>
+                    </div>
+                    <div class="column is-9">
+                        <input type="text" class="input poppins" placeholder="Enter a question">
+                    </div>
+                </div>
+                <div class="columns is-vcentered mx-2">
+                    <div class="column is-2"></div>
+                    <div class="column is-1">
+                        <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                    </div>
+                    <div class="column is-9">
+                        <input type="text" class="input poppins">
+                    </div>
+                </div>
+                <div class="columns is-vcentered mx-2">
+                    <div class="column is-2"></div>
+                    <div class="column is-1">
+                        <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                    </div>
+                    <div class="column is-9">
+                        <input type="text" class="input poppins">
+                    </div>
+                </div>
+                <div class="columns is-vcentered mx-2">
+                    <div class="column is-2"></div>
+                    <div class="column is-1">
+                        <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                    </div>
+                    <div class="column is-9">
+                        <input type="text" class="input poppins">
+                    </div>
+                </div>
+                <div class="columns is-vcentered mx-2">
+                    <div class="column is-2"></div>
+                    <div class="column is-1">
+                        <input type="radio" style="height: 18px !important; width: 18px !important;" name="answer" id="">
+                    </div>
+                    <div class="column is-9">
+                        <input type="text" class="input poppins">
+                    </div>
+                </div>
+            `;
+        },
+        createQuiz: true,
+        showSuccess: false,
+        publishQuiz() {
+            this.createQuiz = ! this.createQuiz;
+            this.showSuccess = ! this.showSuccess;
+        }
     }));
 });
